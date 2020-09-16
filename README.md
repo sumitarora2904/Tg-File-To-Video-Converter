8523237cda7d5a39231aaa0ffdbfd5ed85fe4033
Satyu <70142319+SatyuShree000@users.noreply.github.com>
2020-09-16 14:41:18 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+## [Tg File To Video Converted Creator](https://telegram.dog/shreevish)
+---
+
+
+### Installation
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/SatyuShree000/Tg-File-To-Video-Converter/tree/master)
+
+## Our Telegram Channel and Group
+
+* [AllMovieRockers](https://telegram.dog/allmovierockers)
+
+
+### [@BotFather](https://telegram.dog/BotFather) Commands
+
+```
+start - Check if the Bot is Online!
+help - How to use this Bot?
+upgrade - Upgrade your status
+converttovideo - Convert to Streamable Video
+deletethumbnail - To delete the custom Thumbnail
+```
---
+++ translation.py
@@ -0,0 +1,42 @@
+class Translation(object):
+    START_TEXT = """Hello,
+I am Telegram File-To-Video Converter Bot! Created by @shreevish
+
+Please send me any direct download URL Link, i can upload to telegram as File/Video
+
+ 🚨 . . . NB: its support almost all kind of video files like mp4,mkv..... except shortened videos & some formats . . . 🚨
+ 
+🚨 PRON video🔞 gives you PERMANENT BAN 🚨
+
+       ┈┈┈••💙✿❤️✿💚••┈┈┈
+       
+Bot created by @shreevish"""
+    ABS_TEXT = " Please don't be selfish."
+    BANNED_USER_TEXT = "you are banned"
+    UPGRADE_TEXT = "No upgrade plan 🥰"
+    DOWNLOAD_START = "trying to download"
+    UPLOAD_START = "trying to upload"
+    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/fileConvertRobot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot \nUploaded in {} seconds."
+    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in next 24 hr"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+    CUSTOM_CAPTION_UL_FILE = ""
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    CURENT_PLAN_DETAILS = """Current plan details
+--------
+Telegram ID: <code>{}</code>
+Plan name: <a href='https://t.me/InFoTelGroup/174'>{}</a>
+Expires on: {}"""
+    HELP_USER = """I can do:
+👉 Reply /converttovideo to Convert To Streamable video any telegram file
+--------
+
+Send /me to know current plan details
+
+"""
+
+    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
